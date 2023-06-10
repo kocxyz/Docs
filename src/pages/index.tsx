54ae5ec53e8cb9fbd56a0299abfb9178c26a5a1b
Sky Rose <zgnomdnv@sky.skyqrose.com>
2023-06-10 12:34:13 -0400

    typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,6 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Welcome to Knockout City`}
